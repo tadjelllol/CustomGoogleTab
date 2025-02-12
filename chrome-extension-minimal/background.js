e05bd8a10421ed821d7a2e13f846566335c1ccb1
TadjellTheAlchemist <76237371+tadjelllol@users.noreply.github.com>
2025-02-12 15:21:12 -0800

    more fixes

--- chrome-extension-minimal/background.js
+++ chrome-extension-minimal/background.js
@@ -1,19 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.tabs.create({ url: 'chrome://newtab/' });
-});
-
-chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [1],
-    addRules: [{
-        id: 1,
-        priority: 1,
-        action: { type: 'redirect', redirect: { extensionPath: '/newtab.html' } },
-        condition: { urlFilter: 'chrome://newtab/' }
-    }]
-});
-
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.tabs.onCreated.addListener(tab => {
-        chrome.tabs.update(tab.id, { url: 'chrome://newtab/' });
-    });
+    chrome.tabs.create({ url: 'chrome-extension://' + chrome.runtime.id + '/newtab.html' });
 });
